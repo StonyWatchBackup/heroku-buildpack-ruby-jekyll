2949f4a787317a46d4303398de69716007f8296b
Matthew Tole <matthewtole@gmail.com>
2014-12-17 23:33:11 +0000

    Remove call to Compass

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -710,9 +710,6 @@ params = CGI.parse(uri.query || "")
   end
 
   def generate_jekyll_site
-    puts("Build compass CSS")
-    pipe("env PATH=$PATH vendor/bundle/ruby/1.9.1/bin/compass compile --output-style compressed --force --css-dir source/stylesheets", user_env: true)
-
     puts "Building jekyll site"
     pipe("env PATH=$PATH vendor/bundle/ruby/1.9.1/bin/jekyll build --trace 2>&1", user_env: true)
 
